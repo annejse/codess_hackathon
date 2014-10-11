737678c6858847fd380ae26c78a43a974925a6e7
annejse <annejse@gmail.com>
2014-10-11 11:38:13 +0100

    Add start game button

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 
+function start_game() {
+	$('#start-game').remove()
+
+	start_game_div = jQuery('<div/>', {
+	    id: 'start-game'
+	}).appendTo('#main-container');
+
+	start_game_div.append("<div class='btn btn-primary'>Start Game</div>")
+}
